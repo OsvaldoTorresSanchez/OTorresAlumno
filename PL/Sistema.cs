55a45aef26e8946b63e90fec95b9ae2d8905006d
KDanielRosas <k.daniel.rosas99@gmail.com>
2023-10-13 15:47:06 -0600

    Add project files.

---
+++ PL/Sistema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Sistema
+    {
+        public int IdSistema { get; set; }
+
+        public string Nombre { get; set; }
+
+    }
+
+}
+}
